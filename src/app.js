3801263f25d37e8115c7c92a50dc67cd5da0b018
Romário Lima <devromariolima@gmail.com>
2024-08-30 16:46:23 -0300

    Fix: app.use(routes)

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ import routes from './routes.js'
 
 const app = express()
 
-//usar o router
-app.use(routes)
-
 //indicar para ler dados em json
 app.use(express.json())
 
+//usar o router
+app.use(routes)
 
 export default app
 
